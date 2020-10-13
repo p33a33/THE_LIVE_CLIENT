11c2747c4bdd13ecdfc4ce747c961299a624ba41
조성민 <p33a33@gmail.com>
2020-10-13 13:49:52 +0900

    [Client] Feature : Streaming player added:art:
    * Streaming 중인 영상을 받아보는 Streaming player가 src/pages/Feed/Watching/Watching.js에 추가됐습니다
    
    * 로컬환경 테스트
    1. RTMP 서버를 실행시킵니다.
    2. OBS Studio를 설치하고, 설정-서버탭의 서버에 로컬 RTMP 주소/live/test를 넣어줍니다. (localhost가 안될 경우, 본인의 IPv4 주소를 넣습니다)
    3. Client파일의 src/pages/Feed/Watching/Watching.js의 RMTP_SERVER변수에 로컬 RTMP 주소를 넣어주고 run android를 실행시킵니다.
    4. OBS Studio에서 방송 시작을 누르고, 안드로이드 시뮬레이터에서 로그인-Search-Search버튼 클릭-THUMBNAILS를 클릭하여 Watching 페이지로 이동합니다.
    5. 방송중인 자신의 모습을 감상합니다.

--- src/pages/Feed/Watching/Watching.js
+++ src/pages/Feed/Watching/Watching.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { View } from 'react-native'
 import { Text, Button } from 'react-native-elements'
+import { NodePlayerView } from 'react-native-nodemediaclient'
 
 export default class Watching extends React.Component {
 
     render() {
+        let RTMP_SERVER = 'rtmp://172.30.1.44:4000'
+        let userName = 'test'
+        let inputUrl = `${RTMP_SERVER}/live${userName}`
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>it's Watching Page</Text>
-            </View>
+            <NodePlayerView
+                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+                ref={(vb) => { this.NodePlayerView = vb }}
+                inputUrl={inputUrl}
+                scaleMode="ScaleAspectFit"
+                bufferTime={300}
+                maxBufferTime={1000}
+                autoplay
+            />
         )
     }
 }
