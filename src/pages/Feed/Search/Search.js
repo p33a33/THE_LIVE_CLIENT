cbee0b3007a5d9bf9856c96c12e559c79f0811fd
kimwunhae <kimeunhye1201@gmail.com>
2020-10-14 14:47:46 +0900

    [Updated]Searchd.js ✨

--- src/pages/Feed/Search/Search.js
+++ src/pages/Feed/Search/Search.js
@@ -1,33 +1,70 @@
 import React from 'react'
 import { View, ScrollView, StyleSheet, ListView } from 'react-native'
-import { Text, Button, Image, input } from 'react-native-elements'
+import { Text, Button, Image, Input } from 'react-native-elements'
 import { min } from 'react-native-reanimated';
 import { YOUTUBE_API_KEY } from '../config/youtube';
 import { searchYouTube } from '../searchYouTube';
+
 
 export default class Search extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            subscribeVids: [],
             popularVids: [],
+            popularItms: [],
         }
     }
     componentDidMount() {
         let initOption = {
             query: 'storror',
-            max: 10,
+            max: 6,
             key: YOUTUBE_API_KEY
         }
-        searchYouTube(initOption, (data) => this.setState({ popularVids: data }))
+        let rdmOption = {
+            query: 'boiler+room',
+            max: 6,
+            key: YOUTUBE_API_KEY
+        }
+        Promise.all([searchYouTube(rdmOption, (data) => this.setState({ popularVids: data })), searchYouTube(initOption, (data) => this.setState({ popularItms: data }))])
+
     }
 
     render() {
         return (
             <ScrollView style={styles.body}>
+                <Input style></Input>
+                <View style={styles.searchButton}>
+                    <Button
+                        title="Search"
+                        onPress={() => this.props.navigation.navigate('SearchResult')}
+                    />
+                </View>
                 <Text style={styles.title}>현재 인기있는 방송</Text>
                 <View style={styles.container}>
                     {this.state.popularVids.map((vid) => {
+                        let imgSrc = vid.snippet.thumbnails.default.url
+                        return (
+                            <View className="video-list-entry" style={styles.listItems} >
+                                <View >
+                                    <Image className="media-thumbnail"
+                                        style={{ height: 130, width: 130 }}
+                                        source={{ uri: imgSrc }}
+                                        onPress={() => this.props.navigation.navigate('Watching')}
+                                    />
+                                    <View className="media-body" >
+                                        <Text className="video-list-entry-title" onPress={() => this.props.navigation.navigate('Watching')} >
+                                            {vid.snippet.title}
+                                        </Text>
+                                    </View>
+                                </View>
+                            </View >
+                        )
+                    })
+                    }
+                </View>
+                <Text style={styles.title}>현재 인기있는 제품</Text>
+                <View style={styles.container}>
+                    {this.state.popularItms.map((vid) => {
                         let imgSrc = vid.snippet.thumbnails.default.url
                         return (
                             <View className="video-list-entry" style={styles.listItems} >
@@ -70,6 +107,10 @@ const styles = StyleSheet.create({
         width: "100%",
         alignItems: "flex-start",
         backgroundColor: "#f2f2f2",
+    },
+    searchButton: {
+        width: "50%",
+        marginBottom: 5,
     },
     listItems: {
         padding: 10,
